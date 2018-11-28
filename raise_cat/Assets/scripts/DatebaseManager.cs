ee52bbd64db5452c3a75f8a04377928b2ba88686
Unknown <dbdbdb156@naver.com>
2018-11-29 02:08:55 +0900

    Version Update 로 인한 파일 수정
    Unity 버전을 2.8 에서 17.4.3 으로 Version up 을 함에따라 전 파일의 내용을 새로이 수정함.

---
+++ raise_cat/Assets/scripts/DatebaseManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatebaseManager : MonoBehaviour {
+
+    static public DatebaseManager instance;
+
+	// Use this for initialization
+	private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+
+    public string[] var_name;
+    public float[] var;
+
+    public string[] switch_name;
+    public bool[] switches;
+
+    public List<ToyandItem> itemList = new List<ToyandItem>();
+
+
+    void Start()
+    {
+        itemList.Add(new ToyandItem("item1", 100, "장난감", "장난감들 리스트를 확인하는는 과정입니다.", ToyandItem.ItemType.Toy));
+        itemList.Add(new ToyandItem("item2", 200, "먹이", "특식(먹이) 리스트를 확인하는 과정입니다..", ToyandItem.ItemType.Use));
+
+    }
+
+
+
+
+}
---
+++ raise_cat/Assets/scripts/frontscene.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frontscene : MonoBehaviour {
+
+    // 양육 탭
+    public GameObject walking;
+    public GameObject shop;
+    public GameObject sleep;
+    public GameObject goaway;
+    public GameObject tool;
+    // 양육 & 교감 탭 선정
+    public GameObject R;
+    public GameObject C;
+    
+    // Use this for initialization
+    void Start () {
+        walking = GameObject.Find("walking");
+        shop = GameObject.Find("shoping");
+        sleep = GameObject.Find("sleeping");
+        goaway = GameObject.Find("going");
+        tool = GameObject.Find("tooling");
+
+        R = GameObject.Find("raising_main");
+        C = GameObject.Find("comunicate_main");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+    public void OnClickR()
+    {
+        R.transform.SetAsLastSibling();
+    }
+    public void OnClickC()
+    {
+        C.transform.SetAsLastSibling();
+    }
+    
+    public void OnClickwalking()
+    {
+        walking.transform.SetAsLastSibling();
+    }
+    public void OnClickbackwalking()
+    {
+        walking.transform.SetAsFirstSibling();
+    }
+
+    public void OnClickshop()
+    {
+        shop.transform.SetAsLastSibling();
+    }
+    public void OnClickbackshop()
+    {
+        shop.transform.SetAsFirstSibling();
+    }
+
+    public void OnClicksleep()
+    {
+        sleep.transform.SetAsLastSibling();
+    }
+    public void OnClickbacksleep()
+    {
+        sleep.transform.SetAsFirstSibling();
+    }
+
+    public void OnClickgoaway()
+    {
+        goaway.transform.SetAsLastSibling();
+    }
+    public void OnClickbackgoaway()
+    {
+        goaway.transform.SetAsFirstSibling();
+    }
+
+    public void OnClicktool()
+    {
+        tool.transform.SetAsLastSibling();
+    }
+    public void OnClickbacktool()
+    {
+        tool.transform.SetAsFirstSibling();
+    }
+
+}
